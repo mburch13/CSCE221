c68b0b6a2cf1517dd1091007286c4a79c4527cb6
McKenzie <46796707+mburch13@users.noreply.github.com>
2020-01-09 14:46:11 -0600

    Create matrix.cpp

---
+++ Assingments/Assignment4/finalProject/matrix.cpp
@@ -0,0 +1,67 @@
+//
+//  matrix.cpp
+//  finalProject
+//
+//  Created by McKenzie on 8/6/18.
+//  Copyright © 2018 McKenzie. All rights reserved
+
+/*
+
+class graph
+private:
+ outputfilename
+ ofstream
+ vector<vector<int> > inputInfo
+ vector<vector<int> > adjMatrix
+ vector int visited
+    like stack
+ vector<int> path
+ doub vec char
+    holds text output
+ int n
+ path lenght size of vector path
+ 
+public:
+ void read info
+    read info
+ 
+                 int val_read = 0;
+                 n = 0 //room
+                 for(int i = 0; not end of file; ++i){
+                     vector int row
+                         for(int j =0; j<4 && not end of file; j++){
+                             maze >> val_read
+                             row.push_back(val_read)
+                         }
+                    n++
+                    inputInfo.push_back(row)
+                 }
+ bool dfs
+ print maze
+ 
+ bool tracking
+ print matrix
+ print path
+ print path length
+ outputInfo
+ 
+
+*/
+
+/*------------------------------------------------------------------------------------------------*/
+
+/*
+ menu
+ 
+ 1: print adjacency matrix
+ 2: find lenght of maze path
+    print maze solution
+ 
+    o o x x
+    x o x x
+    o o x x
+    o o o o
+ 
+ 3: DFS
+ 
+ */
